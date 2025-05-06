0d20c18d1874fdb79ff4d330d48b740376a6eff7
Justin <calleja.justin@gmail.com>
2025-05-06 19:31:12 +0200

    .

--- 06-loading-assets/src/scenes/Preloader.js
+++ 06-loading-assets/src/scenes/Preloader.js
@@ -13,17 +13,10 @@ export class Preloader extends Scene {
     const outerBox = this.add
       .rectangle(512, 384, 468, 32)
       .setStrokeStyle(1, 0xffffff);
-    console.log(
-      "outBox is:",
-      outerBox.x,
-      outerBox.y,
-      outerBox.originX,
-      outerBox.originY
-    );
 
     //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
     const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
-    // window.theBar = bar
+    window.theBar = bar
 
     //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
     this.load.on("progress", (progress) => {
