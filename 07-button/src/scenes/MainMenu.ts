a0c1a22a8e6d448034b187778cb80368f1cc9ebb
Justin <calleja.justin@gmail.com>
2025-05-18 11:40:45 +0200

    format

--- 07-button/src/scenes/MainMenu.ts
+++ 07-button/src/scenes/MainMenu.ts
@@ -1,25 +1,27 @@
-import { Scene, GameObjects } from 'phaser';
+import { Scene, GameObjects } from "phaser";
 
-export class MainMenu extends Scene
-{
-    background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
+export class MainMenu extends Scene {
+  background: GameObjects.Image;
+  logo: GameObjects.Image;
+  title: GameObjects.Text;
 
-    constructor ()
-    {
-        super('MainMenu');
-    }
+  constructor() {
+    super("MainMenu");
+  }
 
-    create ()
-    {
-        this.background = this.add.image(512, 384, 'background');
+  create() {
+    this.background = this.add.image(512, 384, "background");
 
-        const width = this.scale.width / 2;
-        const height = this.scale.height / 2;
+    const width = this.scale.width / 2;
+    const height = this.scale.height / 2;
 
-        const flat = this.add.image(width, height, 'flat');
-        const gloss = this.add.image(width, height + flat.height, 'gloss');
-        const gradient = this.add.image(width, height + flat.height * 2, 'gradient');
-    }
+    const flat = this.add.image(width, height, "flat");
+    const gloss = this.add.image(width, height + flat.height, "gloss");
+    gloss.setName("glossButton");
+    const gradient = this.add.image(
+      width,
+      height + flat.height * 2,
+      "gradient"
+    );
+  }
 }
